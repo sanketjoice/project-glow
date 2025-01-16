ca6f97b334959c2563b5a93252c2b3c431a245c4
sanket <joicesanket2@gmail.com>
2025-01-16 09:27:03 +0530

    Update: Added Footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import logo from "../components/Project Glow - Design Assets/Project Glow Logo/PG-Skin logo-dark.svg";
+import facebookLogo from "./Project Glow - Design Assets/Icons/icons8-facebook-logo-64.png";
+import instaLogo from "./Project Glow - Design Assets/Icons/icons8-instagram-logo-30.png";
+import xLogo from "./Project Glow - Design Assets/Icons/icons8-x-logo-50.png";
+import tiktokLogo from "./Project Glow - Design Assets/Icons/icons8-tiktok-50.png";
 
 export default function Footer(){
     return(
@@ -14,13 +18,13 @@ export default function Footer(){
                         <ul className="text-[14px]">Contact Us</ul>
                     </li>
                 </div>
-                <div className="pt-16">
+                <div className="pt-10 flex justify-between">
                     <p className="text-[12px] text-[#9E9E9E] ml-40">©2024 Project Glow Skin Co. All Rights Reserved. </p>
-                    <div>
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+                    <div className="flex gap-2">
+                        <img src={facebookLogo} alt="fblogo" className="w-[36px]" />
+                        <img src={instaLogo} alt="instalogo" className="w-[32px] h-[32px] rounded-full" />
+                        <img src={xLogo} alt="xlogo" className="w-[32px] h-[32px] rounded-full"/>
+                        <img src={tiktokLogo} alt="tiktoklogo" className="w-[32px] h-[32px] rounded-full"/>
                     </div>
                 </div>
         </div>
